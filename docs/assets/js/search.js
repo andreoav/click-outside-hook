c8e9fbbb960b2865bf722a58e46b0cc87d9be148
Andreo Vieira <andreoav@gmail.com>
2019-02-19 17:12:48 -0300

    refactor(project): update project structure

---
+++ docs/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"useClickOutside","url":"globals.html#useclickoutside","classes":"tsd-kind-function"}]};
